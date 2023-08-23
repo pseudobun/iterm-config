227928b0d389a1f85e6d5094633ac716cc35bd5e
Urban Vidovič <urbanfoundit@gmail.com>
2023-08-23 13:05:10 +0200

    chore: update fish_prompt config
    Signed-off-by: Urban Vidovič <urbanfoundit@gmail.com>

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -22,23 +22,24 @@ function fish_prompt
   set -l stash    "="
   set -l none     "✓"
 
-  set -l normal_color     (set_color normal)
-  set -l success_color    (set_color cyan)
-  set -l error_color      (set_color $fish_color_error 2> /dev/null; or set_color red --bold)
-  set -l directory_color  (set_color green)
-  set -l repository_color (set_color yellow)
-  set -l git_icon_color (set_color magenta)
+  set -l primary_color      (set_color normal)
+  set -l secondary_color    (set_color cyan)
+  set -l tertiary_color     (set_color green)
+  set -l error_color        (set_color $fish_color_error 2> /dev/null; or set_color red --bold)
+  set -l repository_color   (set_color yellow)
+  set -l special_color      (set_color magenta)
 
-  set -l prompt_string (whoami)
+  set -l prompt_string (whoami)"$tertiary_color@$special_color"(hostname -s)
 
   if test "$theme_ignore_ssh_awareness" != 'yes' -a -n "$SSH_CLIENT$SSH_TTY"
-    set prompt_string "$fish "(whoami)"@"(hostname -s)
+    # may be used in future
+    set prompt_string (whoami)"@"(hostname -s)
   end
 
   if test $last_command_status -eq 0
-    echo -n -s $success_color $prompt_string $normal_color
+    echo -n -s $secondary_color $prompt_string $primary_color
   else
-    echo -n -s $error_color $prompt_string $normal_color
+    echo -n -s $error_color $prompt_string $primary_color
   end
 
   if git_is_repo
@@ -48,8 +49,8 @@ function fish_prompt
       set cwd (echo $PWD | sed -e "s|$parent_root_folder/||")
     end
 
-    echo -n -s " " $normal_color \[ $directory_color $cwd $normal_color \]
-    echo -n -s "$git_icon_color  " $repository_color (git_branch_name) $normal_color " "
+    echo -n -s " " $primary_color \[ $tertiary_color $cwd $primary_color \]
+    echo -n -s "$special_color  " $repository_color (git_branch_name) $primary_color " "
 
 
     set -l list
@@ -65,8 +66,8 @@ function fish_prompt
       echo -n -s (git_ahead $ahead $behind $diverged $none)
     end
   else
-    echo -n -s " " $normal_color \[ $directory_color $cwd $normal_color \]
+    echo -n -s " " $primary_color \[ $tertiary_color $cwd $primary_color \]
   end
 
-  echo -n -s $success_color " $fish "
+  echo -n -s $secondary_color " $fish "
 end
