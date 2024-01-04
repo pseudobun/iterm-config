b18dbb793dad3c6ea55428682570c8702e089709
Urban Vidovič <urbanfoundit@gmail.com>
2024-01-04 12:13:43 +0100

    chore: yabai updates

--- sketchybar/plugins/yabai.sh
+++ sketchybar/plugins/yabai.sh
@@ -37,6 +37,27 @@ window_state() {
   sketchybar -m "${args[@]}"
 }
 
+windows_on_spaces () {
+  CURRENT_SPACES="$(yabai -m query --displays | jq -r '.[].spaces | @sh')"
+
+  args=()
+  while read -r line
+  do
+    for space in $line
+    do
+      icon_strip=" "
+      apps=$(yabai -m query --windows --space $space | jq -r ".[].app")
+      if [ "$apps" != "" ]; then
+        while IFS= read -r app; do
+          icon_strip+=" $($HOME/.config/sketchybar/plugins/icon_map.sh "$app")"
+        done <<< "$apps"
+      fi
+      args+=(--set space.$space label="$icon_strip" label.drawing=on)
+    done
+  done <<< "$CURRENT_SPACES"
+
+  sketchybar -m "${args[@]}"
+}
 
 mouse_clicked() {
   yabai -m window --toggle float
@@ -46,6 +67,10 @@ mouse_clicked() {
 case "$SENDER" in
   "mouse.clicked") mouse_clicked
   ;;
+  "forced") exit 0
+  ;;
   "window_focus") window_state 
+  ;;
+  "windows_on_spaces") windows_on_spaces
   ;;
 esac
