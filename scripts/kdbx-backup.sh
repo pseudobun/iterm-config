36bcaa92c656454d88810f44096bba453a48916f
Urban Vidovič <urbanfoundit@gmail.com>
2024-03-08 18:08:36 +0100

    feat(ntfy): notify on successful kdbx backups
    Signed-off-by: Urban Vidovič <urbanfoundit@gmail.com>

--- scripts/kdbx-backup.sh
+++ scripts/kdbx-backup.sh
@@ -3,3 +3,9 @@ cd ~/Library/Mobile\ Documents/iCloud~com\~strongbox/Documents/kdbxs
 git add --all
 git commit -aS -m "chore: backup $(date +"%d-%m-%Y %H:%M:%S")"
 git push
+
+if [ $? -eq 0 ]; then
+    ntfy publish --title="KDBX backup" --priority=low --tags=partying_face kdbx-backup "KDBXs backed up successfully"
+else
+    ntfy publish --title="KDBX backup" --priority=high --tags=sob kdbx-backup "KDBXs failed to back up"
+fi
