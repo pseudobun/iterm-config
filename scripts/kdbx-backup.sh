00fb204ee47ebc767c8702df717fc781b62e5dbe
Urban Vidovič <urbanfoundit@gmail.com>
2024-07-02 16:01:34 +0200

    chore: update backups script
    Signed-off-by: Urban Vidovič <urbanfoundit@gmail.com>

--- scripts/kdbx-backup.sh
+++ scripts/kdbx-backup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cd ~/Library/Mobile\ Documents/iCloud~com\~strongbox/Documents/kdbxs
+cd "~/Google Drive/My Drive/1-personal/1-kdbxs/kdbxs"
+touch test.txt
 git add --all
 git commit -aS -m "chore: backup $(date +"%d-%m-%Y %H:%M:%S")"
 git push
