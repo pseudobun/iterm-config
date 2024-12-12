f329860a284bf51b81217c5ed6fc420a44b47aef
Urban Vidovič <urbanfoundit@gmail.com>
2024-12-12 01:03:22 +0100

    feat(Brewfile): switch to uv from pyenv
    Signed-off-by: Urban Vidovič <urbanfoundit@gmail.com>

--- setup.fish
+++ setup.fish
@@ -2,10 +2,10 @@ echo "Setting up omf..."
 omf install bass
 omf theme pseudobun
 echo "Finished omf setup."
-echo "Setting up pyenv..."
-set -Ux PYENV_ROOT $HOME/.pyenv
-fish_add_path $PYENV_ROOT/bin
+# echo "Setting up pyenv..."
+# set -Ux PYENV_ROOT $HOME/.pyenv
+# fish_add_path $PYENV_ROOT/bin
 fish_add_path /opt/homebrew/opt/postgresql@15/bin
 fish_add_path ~/.cargo/bin
-echo "Finished pyenv setup."
+# echo "Finished pyenv setup."
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
